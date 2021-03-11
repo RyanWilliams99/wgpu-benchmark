398cefec6be4602b65010c18511202a79f78360b
Ryan Williams <ryanwilliamsdesktop@outlook.com>
2021-03-11 17:15:30 +0000

    Only render one instance of model

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
     0.0, 0.0, 0.5, 1.0,
 );
 
-const NUM_INSTANCES_PER_ROW: u32 = 10;
+const NUM_INSTANCES_PER_ROW: u32 = 1;
 
 struct Camera {
     eye: cgmath::Point3<f32>,
